edfecadd75975a0217999ae2c8af3fc73f1a0bd8
word4010 <ml.mykola.word.11@gmail.com>
2026-02-12 18:03:05 +0200

    add code

---
+++ js/01work.js
@@ -0,0 +1,34 @@
+const myAge = 12;
+console.log(myAge);
+
+const myName = "Mykola";
+console.log(myName);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Вчиться той - хто не лінується";
+console.log(myString);
+
+const myNumber = +10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+
+let userName = prompt("Введіть своє ім’я");
+
+alert("Ваше ім’я: " + userName);
+
+console.log("Тип змінної userName:", typeof userName);
+
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+
+
+console.log("Відповідь користувача:", userAnswer);
+
+alert("Дія може бути не безпечною");
+confirm("Ви впевнені, що хочите продовжити?");
+
