1bf28f959a2a4532f1a3095859ef36ffc91b93be
word4010 <ml.mykola.word.11@gmail.com>
2026-07-26 11:56:14 +0300

    hm15

--- js/hm14-15.js
+++ js/hm14-15.js
@@ -123,3 +123,41 @@ const getUserWithEmail = (users, email) => {
 console.log(getUserWithEmail(users, "shereeanthony@kog.com"));
 console.log(getUserWithEmail(users, "elmahead@omatom.com"));
 
+
+
+// 1
+const calculateTotalBalance = (users) => {
+    return users.reduce((total, user) => total + user.balance, 0);
+};
+
+console.log(calculateTotalBalance(users)); 
+
+// 2
+const getUsersWithFriend = (users, friendName) => {
+    return users
+        .filter(user => user.friends.includes(friendName))
+        .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, "Briana Decker"));
+
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+
+
+
+//  3
+const getNamesSortedByFriendsCount = (users) => {
+    return users
+        .slice()
+        .sort((a, b) => a.friends.length - b.friends.length)
+        .map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+
+//  4
+const getSortedUniqueSkills = (users) => {
+    return [...new Set(users.flatMap(user => user.skills))].sort();
+};
+
+console.log(getSortedUniqueSkills(users));
